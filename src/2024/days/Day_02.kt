61adfcae129d18a0b09c2a934a67b47660983092
Denis Schüle <schueledenis@gmx.at>
2025-11-23 20:15:19 +0100

    aoc-02: init

---
+++ src/2024/days/Day_02.kt
@@ -0,0 +1,37 @@
+package `2024`.days
+
+import readInput
+
+
+/**
+ * Advent of code (2024) solution for day 02 by Denis Schüle.
+ * [Advent of code 2024-02 ](https://adventofcode.com/2024/day/02)
+ **/
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    try {
+
+        val input = readInput("day_02", "2024")
+
+        val part1_solution = part1(input)
+        println("Part 1: ")
+//        check(part1_solution == 1)
+
+        val part2_solution = part2(input)
+
+        println("Part 2: ")
+//        check(part2_solution == 1)
+
+    } catch (t: Throwable) {
+        t.printStackTrace()
+        throw t
+    }
+}
