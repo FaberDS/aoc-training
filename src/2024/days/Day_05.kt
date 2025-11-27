e5bb99efdfcbf8c5c29c916fc5e7e207a9e6554a
Denis Schüle <34751666+FaberDS@users.noreply.github.com>
2025-11-28 00:20:27 +0100

    Aoc/2024/05 (#5)
    * aoc-2024-05: setup
    
    * aoc-2024-05: completed task 1
    
    - stuck on task 2
    
    * aoc-2024-05: completed part 2
    
    ---------
    
    Co-authored-by: GitHub Actions Bot <github-actions[bot]@users.noreply.github.com>

---
+++ src/2024/days/Day_05.kt
@@ -0,0 +1,133 @@
+/**
+ * Advent of code (2024) solution for day 5 by Denis Schüle.
+ * [Advent of code 2024-5 ](https://adventofcode.com/2024/day/5)
+ **/
+ import extensions.middleValue
+ import extensions.printSeparated
+ import jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle
+ import jdk.jfr.internal.consumer.EventLog.update
+ import java.util.Collections
+ import kotlin.time.measureTimedValue
+
+
+fun main() {
+
+    val exampleSolution1 = 143
+    val exampleSolution1RowCount = 3
+    val exampleSolution2 = 123
+
+    fun splitInput(input: List<String>): Pair<List<Pair<Int,Int>>,List<List<Int>>> {
+        val orderList = mutableListOf<Pair<Int, Int>>()
+        val updateList = mutableListOf<List<Int>>()
+        for (line in input) {
+            if (line.contains('|')) {
+                val splitted = line.split('|')
+                                        .map { it.toInt() }
+                orderList.add(Pair(splitted.first(), splitted.last()))
+            }
+            if (line.contains(',')) {
+                updateList.add(line.split(',').map { it.toInt() }.toList())
+            }
+        }
+        return Pair(orderList,updateList)
+    }
+    fun validateRow(orderRules: List<Pair<Int,Int>>, pageToIndex: Map<Int,Int>): Boolean {
+        var orderIsValid = true
+
+        for ((predecessor, successor) in orderRules) {
+            val indexPre = pageToIndex[predecessor]
+            val indexSucc = pageToIndex[successor]
+
+            if (indexPre != null && indexSucc != null) {
+                if (indexPre >= indexSucc) {
+                    orderIsValid = false
+                    break
+                }
+            }
+        }
+        return orderIsValid
+    }
+    fun filterUpdateLists(orderRules: List<Pair<Int,Int>>,updateList:List<List<Int>>, filterValid: Boolean): List<List<Int>> {
+        val filteredUpdate = mutableListOf<List<Int>>()
+        for (update in updateList) {
+            val pageToIndex = update.withIndex().associate { it.value to it.index }
+
+            var orderIsValid = validateRow(orderRules,pageToIndex)
+            if (orderIsValid == filterValid) {
+                filteredUpdate.add(update)
+            }
+        }
+        return filteredUpdate
+    }
+
+    fun part1(input: List<String>): Int {
+        val (orderRules, updateList) = splitInput(input)
+        return filterUpdateLists(orderRules, updateList, true)
+            .sumOf { it.middleValue() }
+    }
+
+    fun buildRuleMap(rules: List<Pair<Int,Int>>): Map<Int,MutableSet<Int>>{
+        val map = mutableMapOf<Int,MutableSet<Int>>()
+        for ((a,b) in rules) {
+            map.getOrPut(a) { mutableSetOf() }.add(b)
+        }
+        return map
+    }
+
+    fun fixUpdate(update: List<Int>, rules: Map<Int,MutableSet<Int>>): List<Int> {
+        val comparator = Comparator<Int> { a,b ->
+            when {
+                rules[a]?.contains(b) == true -> -1
+                rules[b]?.contains(a) == true -> 1
+                else -> 0
+            }
+        }
+        return update.sortedWith(comparator)
+    }
+    fun part2(input: List<String>): Int {
+        val (orderRules, updateList) = splitInput(input)
+        val invalidUpdates = filterUpdateLists(orderRules, updateList, false)
+        println("orderRules: $orderRules")
+        val ruleMap = buildRuleMap(orderRules)
+        return invalidUpdates
+            .map { fixUpdate(it, ruleMap) }
+            .sumOf { it.middleValue() }
+    }
+
+    try {
+
+        /* --- TEST DEMO INPUT --- */
+        val exampleInput = readInput("day_05_demo", "2024")
+        val part1_demo_solution = part1(exampleInput)
+        "Part 1 Demo".printSeparated()
+        println("- Part 1 Demo: $part1_demo_solution")
+        check(part1_demo_solution == exampleSolution1)
+
+
+        "Part 2 Demo".printSeparated()
+        val part2_demo_solution = part2(exampleInput)
+        println("- Part 2 Demo: $part2_demo_solution")
+        check(part2_demo_solution == exampleSolution2)
+
+        /* --- RUN FULL INPUT --- */
+
+        "Part 1".printSeparated()
+        val (part1_solution, timeTakenPart1) = measureTimedValue {
+            val input = readInput("day_05", "2024")
+            part1(input)
+        }
+        println("- Part 1: ${part1_solution} in $timeTakenPart1")
+        check(part1_solution == 4996)
+
+        "Part 2".printSeparated()
+        val (part2_solution, timeTakenPart2) = measureTimedValue {
+            val input = readInput("day_05", "2024")
+            part2(input)
+        }
+        println("- Part 2: ${part2_solution} in $timeTakenPart2")
+        check(part2_solution == 6311)
+
+    } catch (t: Throwable) {
+        t.printStackTrace()
+    }
+}
